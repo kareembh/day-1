562846d8e2951e91a2679b64fe83ba4a2c441dce
kareem <kareembhenry@gmail.com>
2020-09-12 21:28:04 -0400

    added key press function to play track

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles.css";
-console.log("hello world!");
+
+
+window.addEventListener('keydown',function(e){
+    const track = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!track) return;
+    track.play();
+    console.log(track);
+});
