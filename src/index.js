fd0ef39aa5ecde898a0f94d0b0842763c4a65d2d
kareem <kareembhenry@gmail.com>
2020-09-16 09:51:10 -0400

    added styling to keys and added animation.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./styles.css";
 
-
+//adds an event listener to the current window
 window.addEventListener('keydown',function(e){
-    const track = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if(!track) return;
-    track.play();
-    console.log(track);
+    const track = document.querySelector(`audio[data-key="${e.keyCode}"]`); //selects sound associated with specified key
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //selects key 
+    if(!track) return; 
+    track.currentTime = 0; // rewinds track to the start
+    track.play(); //plays the track
+    key.classList.add('playing');
 });
