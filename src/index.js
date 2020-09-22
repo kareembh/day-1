7e841db7b78aa32302fc5250a0d9a8ab71e99843
kareem <kareembhenry@gmail.com>
2020-09-22 19:27:08 -0400

    changed transistion to animationgit status

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import "./styles.css";
 
-//adds an event listener to the current window
-window.addEventListener('keydown',function(e){
+function playSound(e){
     const track = document.querySelector(`audio[data-key="${e.keyCode}"]`); //selects sound associated with specified key
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //selects key 
     if(!track) return; 
     track.currentTime = 0; // rewinds track to the start
     track.play(); //plays the track
     key.classList.add('playing');
-});
+}
+   
+
+function removeTransition(e){
+    this.classList.remove('playing'); //removes playing class 
+}
+
+const keys =document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('animationend', removeTransition));
+
+
+window.addEventListener('keydown',playSound);
+
+
