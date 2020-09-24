8e7ae1182b49f517e4f983d842db33878f2aa66b
kareem <kareembhenry@gmail.com>
2020-09-24 08:14:20 -0400

    added some new comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./styles.css";
 
 function playSound(e){
-    const track = document.querySelector(`audio[data-key="${e.keyCode}"]`); //selects sound associated with specified key
+    const track = document.querySelector(`audio[data-key="${e.keyCode}"]`); //selects sound associated with key using template strings
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //selects key 
     if(!track) return; 
     track.currentTime = 0; // rewinds track to the start
     track.play(); //plays the track
-    key.classList.add('playing');
+    key.classList.add('playing'); //add playing class 
 }
    
 
@@ -15,9 +15,9 @@ function removeTransition(e){
 }
 
 const keys =document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('animationend', removeTransition));
+keys.forEach(key => key.addEventListener('animationend', removeTransition)); //adds an event listener to every key associated with a sound
 
 
-window.addEventListener('keydown',playSound);
+window.addEventListener('keydown',playSound); //adds an event listener to the window to listen for key down event.
 
 
